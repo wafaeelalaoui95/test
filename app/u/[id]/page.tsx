1e5ed6456101de7600976742f31f24280f488a91
wafaeelalaoui95 <wafae.elalaoui95@gmail.com>
2026-05-20 15:20:42 +0400

    Update page.tsx

--- app/u/[id]/page.tsx
+++ app/u/[id]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState, use } from 'react';
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { motion, AnimatePresence } from 'framer-motion';
 import {
